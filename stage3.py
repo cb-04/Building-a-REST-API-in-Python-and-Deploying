03b38630941bfd83748ffd6eecaaf2149e8de0fc
Chandrabali Biswas <chandrabalibiswas04@gmail.com>
2026-01-19 02:09:00 +0530

    Query params

--- stage3.py
+++ stage3.py
@@ -1,13 +1,30 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
+
+courses = [
+    {"course_name": "Python"},
+    {"course_name": "NodeJS"},
+    {"course_name": "Machine Learning"}
+]
+subjects = [
+    {"name": "Maths"},
+    {"name": "Science"},
+    {"name": "Programming"}
+]
 
 @app.get("/")
 def root():
     return {"message":"Hello world"}
 
 
-@app.get("/courses/{course_id}")
-def read_course(course_id:int):
-    return {"course_id": course_id}
+@app.get("/courses")
+def read_course(start:int=0, end:int=10):
+    return courses[start:start+end]
     
+@app.get("/subjects/{subject_id}")
+def read_subjects(subject_id:int, q:Optional[str]=None):
+    if q is not None:
+        return {"subject_name": subjects[subject_id], "q": q}
+    return {"subject_name": subjects[subject_id]}
